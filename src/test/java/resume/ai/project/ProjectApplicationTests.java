27d569f751d2ee38f56681c1fbb99c3ae29ae4ab
codcook <60304062@udst.edu.qa>
2026-04-06 13:30:28 +0300

    Initialize project structure with Maven, including wrapper scripts and configuration files

---
+++ src/test/java/resume/ai/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package resume.ai.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
